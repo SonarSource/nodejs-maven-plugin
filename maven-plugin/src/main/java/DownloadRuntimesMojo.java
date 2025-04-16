7d0f9bb81e61a1ee3e62a468af4191de8be317a7
zglicz <michal.zgliczynski@sonarsource.com>
2025-04-16 11:11:07 +0200

    Fixup and newer cache (#2)
    * fixup and newer cache
    
    * retry with old cache

--- maven-plugin/src/main/java/DownloadRuntimesMojo.java
+++ maven-plugin/src/main/java/DownloadRuntimesMojo.java
@@ -111,16 +111,15 @@ public class DownloadRuntimesMojo extends AbstractMojo {
       FileChannel fileChannel;
       try (var fileOutputStream = new FileOutputStream(destinationFile.toString())) {
         fileChannel = fileOutputStream.getChannel();
-      }
+        getLog().info("Downloading to " + destinationFile);
 
-      getLog().info("Downloading to " + destinationFile);
+        fileChannel.transferFrom(readableByteChannel, 0, Long.MAX_VALUE);
 
-      fileChannel.transferFrom(readableByteChannel, 0, Long.MAX_VALUE);
+        var md5 = DigestUtils.sha256Hex(new FileInputStream(destinationFile.toFile()));
 
-      var md5 = DigestUtils.sha256Hex(new FileInputStream(destinationFile.toFile()));
-
-      if (!md5.equals(flavor.getChecksum())) {
-        throw new ChecksumException(String.format("Invalid checksum for %s", flavor.getUrl()));
+        if (!md5.equals(flavor.getChecksum())) {
+          throw new ChecksumException(String.format("Invalid checksum for %s", flavor.getUrl()));
+        }
       }
     }
   }
